f4e04c13ab4fe9f40e5722bab127499bf9e1bb32
vbhosle <bhosleviraj@gmail.com>
2019-04-14 17:57:27 +0530

    Added  formatting

--- src/main/java/com/interviewbit/LargestNumberFromIntList.java
+++ src/main/java/com/interviewbit/LargestNumberFromIntList.java
@@ -6,28 +6,29 @@ import java.util.Comparator;
 import java.util.List;
 
 public class LargestNumberFromIntList {
-	
+
 	public String largestNumber(final List<Integer> A) {
-		if(A == null || A.isEmpty()) return "";
-		if(A.stream().allMatch(i -> i == 0))
+		if (A == null || A.isEmpty())
+			return "";
+		if (A.stream().allMatch(i -> i == 0))
 			return "0";
 		int len = A.size();
 		List<String> B = new ArrayList<>(len);
-		for(int i = 0; i< len; i++) {
-			B.add(""+A.get(i));
+		for (int i = 0; i < len; i++) {
+			B.add("" + A.get(i));
 		}
 		B.sort(new CustomStringComparator().reversed());
 		return String.join("", B.toArray(new String[0]));
 	}
 
-	static class CustomStringComparator implements Comparator<String>{
+	static class CustomStringComparator implements Comparator<String> {
 
 		@Override
 		public int compare(String s1, String s2) {
-			BigInteger int1 = new BigInteger(s1+s2);
-			BigInteger int2 = new BigInteger(s2+s1);
+			BigInteger int1 = new BigInteger(s1 + s2);
+			BigInteger int2 = new BigInteger(s2 + s1);
 			return int1.compareTo(int2);
 		}
-		
+
 	}
 }
--- src/test/java/com/interviewbit/LargestNumberFromIntListTest.java
+++ src/test/java/com/interviewbit/LargestNumberFromIntListTest.java
@@ -12,7 +12,7 @@ import org.junit.Test;
 public class LargestNumberFromIntListTest {
 
 	private LargestNumberFromIntList solver;
-	
+
 	@Before
 	public void setUp() throws Exception {
 		solver = new LargestNumberFromIntList();
@@ -22,22 +22,22 @@ public class LargestNumberFromIntListTest {
 	public void nullListReturnsEmpty() {
 		assertThat(solver.largestNumber(null), equalTo(""));
 	}
-	
+
 	@Test
 	public void emptyListReturnsEmpty() {
 		assertThat(solver.largestNumber(Collections.emptyList()), equalTo(""));
 	}
-	
+
 	@Test
 	public void _0_0_0_0Returns0() {
 		assertThat(solver.largestNumber(Arrays.asList(0, 0, 0, 0)), equalTo("0"));
 	}
-	
+
 	@Test
 	public void _5Returns5() {
 		assertThat(solver.largestNumber(Arrays.asList(5)), equalTo("5"));
 	}
-	
+
 	@Test
 	public void _3_30_34_5_9Returns9534330() {
 		assertThat(solver.largestNumber(Arrays.asList(3, 30, 34, 5, 9)), equalTo("9534330"));
@@ -47,12 +47,12 @@ public class LargestNumberFromIntListTest {
 	public void _3_30_34_5_9_300Returns9534330300() {
 		assertThat(solver.largestNumber(Arrays.asList(3, 30, 34, 5, 9, 300)), equalTo("9534330300"));
 	}
-	
+
 	@Test
 	public void _0_0_10_100_1000Returns10100100000() {
 		assertThat(solver.largestNumber(Arrays.asList(0, 0, 10, 100, 1000)), equalTo("10100100000"));
 	}
-	
+
 	@Test
 	public void _2147483647_2147483647Returns21474836472147483647() {
 		assertThat(solver.largestNumber(Arrays.asList(2147483647, 2147483647)), equalTo("21474836472147483647"));
