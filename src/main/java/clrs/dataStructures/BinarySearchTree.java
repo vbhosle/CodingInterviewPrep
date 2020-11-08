3bd3b5dcbe60cfc227dd6938426045b69a50714d
vbhosle <bhosleviraj@gmail.com>
2020-11-08 17:49:46 +0530

    BinarySearchTree implementation

--- src/main/java/clrs/dataStructures/BinarySearchTree.java
+++ src/main/java/clrs/dataStructures/BinarySearchTree.java
@@ -1,75 +1,71 @@
 package clrs.dataStructures;
 
 import java.util.List;
-
-import static java.util.Objects.isNull;
-
 import java.util.Deque;
 import java.util.LinkedList;
 
 public class BinarySearchTree {
-	
+
 	Node root = null;
-	
-	//implementation Order - 1
+
+	// implementation Order - 1
 	public boolean isEmpty() {
-		return isNull(root);
+		return root == null;
 	}
-	
-	//implementation Order - 1
-	public void insert(int item) {
+
+	// implementation Order - 1
+	public void insert(Integer key) {
 		Node parent = null;
 		Node current = root;
-		Node newNode = new Node(item);
-		
-		while(!isNull(current)) {
+		Node newNode = new Node(key);
+
+		while (current != null) {
 			parent = current;
-			if(newNode.item < current.item)
+			if (newNode.key < current.key)
 				current = current.left;
 			else
 				current = current.right;
 		}
-		
+
 		newNode.parent = parent;
-		
-		if(isNull(parent)) {
+
+		if (parent == null) {
 			root = newNode;
-		}
-		else if(newNode.item < parent.item) {
+		} else if (newNode.key < parent.key) {
 			parent.left = newNode;
-		}
-		else {
+		} else {
 			parent.right = newNode;
 		}
 	}
-	
-	//implementation Order - 1
-	//We could maintain size member variable for efficiency, but it will complicate code and deviate it a lot from book
+
+	// implementation Order - 1
+	// We could maintain size member variable for efficiency, but it will complicate
+	// code and deviate it a lot from book
 	public int size() {
 		return size(root);
 	}
-	
+
 	private int size(Node node) {
-		if(node == null)
+		if (node == null)
 			return 0;
 		return 1 + size(node.left) + size(node.right);
 	}
 
-	//implementation Order - 1
+	// implementation Order - 1
 	public List<Integer> inorder() {
-		List<Integer> items = new LinkedList<>();
-		inorder(root, items);
-		
-		return items;
+		List<Integer> keys = new LinkedList<>();
+		inorder(root, keys);
+
+		return keys;
 	}
-	
-	private void inorder(Node node, List<Integer> items) {
-		if(isNull(node))
+
+	private void inorder(Node current, List<Integer> visited) {
+		if (current == null)
 			return;
-		
-		inorder(node.left, items);
-		items.add(node.item);
-		inorder(node.right, items);
+
+		inorder(current.left, visited);
+		visited.add(current.key);
+		inorder(current.right, visited);
 	}
 
 	public List<Integer> inorderIterative() {
@@ -77,48 +73,217 @@ public class BinarySearchTree {
 		Deque<Node> stack = new LinkedList<>();
 		Node current = root;
 		Node top = null;
-		
-		while(!stack.isEmpty() || current != null) {
-			if(current != null) {
+
+		while (!stack.isEmpty() || current != null) {
+			if (current != null) {
 				stack.push(current);
 				current = current.left;
-			}
-			else {
+			} else {
 				top = stack.pop();
-				result.add(top.item);
+				result.add(top.key);
 				current = top.right;
 			}
 		}
-		
+
 		return result;
 	}
-	
-	public List<Integer> preorder(int item) {
+
+	//TODO
+	// Morris inorder tree traversal
+	// https://www.youtube.com/watch?v=wGXB9OWhPTg
+	public List<Integer> morrisinorder() {
 		return null;
 	}
-	
-	public List<Integer> postorder(int item) {
+
+	public List<Integer> preorder() {
+		List<Integer> visited = new LinkedList<>();
+		preorder(root, visited);
+		return visited;
+	}
+
+	private void preorder(Node current, List<Integer> visited) {
+		if (current == null)
+			return;
+		visited.add(current.key);
+		preorder(current.left, visited);
+		preorder(current.right, visited);
+	}
+
+	public List<Integer> postorder() {
+		List<Integer> visited = new LinkedList<>();
+		postorder(root, visited);
+		return visited;
+	}
+
+	private void postorder(Node current, List<Integer> visited) {
+		if (current == null)
+			return;
+		postorder(current.left, visited);
+		postorder(current.right, visited);
+		visited.add(current.key);
+	}
+
+	public boolean contains(Integer key) {
+		return contains(root, key);
+	}
+
+	// search
+	private boolean contains(Node current, int key) {
+		if (current == null)
+			return false;
+
+		if (key == current.key)
+			return true;
+
+		if (key < current.key)
+			return contains(current.left, key);
+		else
+			return contains(current.right, key);
+	}
+
+	public boolean containsIterative(int key) {
+		return searchNode(key) != null;
+	}
+
+	private Node searchNode(int key) {
+		Node current = root;
+
+		while (current != null) {
+			if (key == current.key)
+				return current;
+
+			if (key < current.key)
+				current = current.left;
+			else
+				current = current.right;
+		}
+
 		return null;
 	}
-	
-	public Node search(int item) {
-		return null;
+
+	public int min() {
+		if (size() == 0)
+			throw new IllegalArgumentException("tree is empty");
+
+		return min(root).key;
+	}
+
+	private Node min(Node current) {
+		while (current.left != null)
+			current = current.left;
+
+		return current;
+	}
+
+	public int max() {
+		if (size() == 0)
+			throw new IllegalArgumentException("tree is empty");
+
+		return max(root).key;
 	}
 	
-	public Node searchIterative(int item) {
-		return null;
+	private Node max(Node current) {
+		while (current.right != null)
+			current = current.right;
+
+		return current;
+	}
+
+	public Integer successor(int key) {
+		Node successorNode = successor(searchNode(key));
+		return successorNode != null ? successorNode.key : null;
 	}
 	
+	private Node successor(Node node) {
+		if (node == null)
+			return null;
+
+		if (node.right != null) {
+			return min(node.right);
+		} else {
+			Node parent = node.parent;
+
+			while (parent != null && parent.right == node) {
+				node = parent;
+				parent = node.parent;
+			}
+
+			return parent;
+		}
+	}
+
+	public Integer predecessor(int key) {
+		Node current = searchNode(key);
+
+		if (current == null)
+			return null;
+
+		if (current.left != null) {
+			Node maxInLeftTree = max(current.left);
+			return maxInLeftTree != null ? maxInLeftTree.key : null;
+		} else {
+			Node parent = current.parent;
+
+			while (parent != null && parent.left == current) {
+				current = parent;
+				parent = current.parent;
+			}
+
+			return parent != null ? parent.key : null;
+		}
+	}
 	
+	public void delete(int key) {
+		Node nodeToDelete = searchNode(key);
+		
+		if(nodeToDelete == null)
+			return;
+		
+		if(nodeToDelete.left == null) {
+			transplant(nodeToDelete, nodeToDelete.right);
+		}
+		else if(nodeToDelete.right == null) {
+			transplant(nodeToDelete, nodeToDelete.left);
+		}
+		else {
+			Node replacement = min(nodeToDelete);
+			if(replacement.parent != nodeToDelete) {
+				transplant(replacement, replacement.right);
+				replacement.right = nodeToDelete.right;
+				replacement.right.parent = replacement;
+			}
+			
+			transplant(nodeToDelete, replacement);
+			replacement.left = nodeToDelete.left;
+			replacement.left.parent = replacement;
+		}
+	}
+
+	private void transplant(Node u, Node v) {
+		if(u.parent == null) {
+			root = v;
+		}
+		else {
+			if(u == u.parent.left) {
+				u.parent.left = v;
+			}
+			else {
+				u.parent.right = v;
+			}
+		}
+		
+		if(v != null)
+			v.parent = u.parent;
+	}
 	
 	static class Node {
-		int item;
+		Integer key;
 		Node left;
 		Node right;
 		Node parent;
-		
-		Node(int item) {
-			this.item = item;
+
+		Node(Integer key) {
+			this.key = key;
 		}
 	}
 }
--- src/test/java/clrs/dataStructures/BinarySearchTreeTest.java
+++ src/test/java/clrs/dataStructures/BinarySearchTreeTest.java
@@ -3,14 +3,19 @@ package clrs.dataStructures;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.junit.Assert.*;
+import static java.util.Arrays.asList;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
+import java.util.ListIterator;
+import java.util.Map;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
+import org.apache.commons.lang3.tuple.Pair;
 import org.junit.Test;
 
 public class BinarySearchTreeTest {
@@ -21,26 +26,26 @@ public class BinarySearchTreeTest {
 		this.tree = new BinarySearchTree();
 	}
 
-	private void initializedWith(List<Integer> items) {
-		for (int item : items) {
-			tree.insert(item);
+	private void initializedWith(List<Integer> keys) {
+		for (Integer key : keys) {
+			tree.insert(key);
 		}
 	}
 
-	private void traversingReturns(Function<BinarySearchTree, List<Integer>> traverser, List<Integer> items) {
+	private void traversingReturns(Function<BinarySearchTree, List<Integer>> traverser, List<Integer> keys) {
 		List<Integer> traversedItems = traverser.apply(tree);
-		assertThat("traversed items has same size as tree items", traversedItems, hasSize(tree.size()));
-		assertThat("traversed in expected order", traversedItems, equalTo(items));
+		assertThat("traversed keys has same size as tree keys", traversedItems, hasSize(tree.size()));
+		assertThat("traversed in expected order", traversedItems, equalTo(keys));
 	}
 
 	private List<Integer> randomList(int size) {
-		List<Integer> items = new ArrayList<>();
+		List<Integer> keys = new ArrayList<>();
 
-		for (int j = 1; j <= size; j++) {
-			int item = ThreadLocalRandom.current().nextInt(1, size + 1);
-			items.add(item);
+		for (Integer j = 1; j <= size; j++) {
+			Integer key = ThreadLocalRandom.current().nextInt(1, size + 1);
+			keys.add(key);
 		}
-		return items;
+		return keys;
 	}
 
 	@Test
@@ -68,36 +73,179 @@ public class BinarySearchTreeTest {
 
 		for (int i = 1; i <= 5; i++) {
 			int size = ThreadLocalRandom.current().nextInt(MIN_SIZE, MAX_SIZE + 1);
-			List<Integer> items = randomList(size);
+			List<Integer> keys = randomList(size);
 
-			testInsert(items);
+			testInsert(keys);
 		}
 	}
 
-	public void testInsert(List<Integer> items) {
+	public void testInsert(List<Integer> keys) {
 		givenATree();
-		initializedWith(items);
+		initializedWith(keys);
 
-		assertThat("tree size is same as items", tree.size(), equalTo(items.size()));
+		assertThat("tree size is same as keys", tree.size(), equalTo(keys.size()));
 
-		Collections.sort(items);
-		traversingReturns(BinarySearchTree::inorder, items);
+		Collections.sort(keys);
+		traversingReturns(BinarySearchTree::inorder, keys);
 	}
 
 	@Test
-	public void testInorderIterative() {
+	public void inorderIterative() {
 		final int MIN_SIZE = 6;
 		final int MAX_SIZE = 10;
 
 		for (int i = 1; i <= 5; i++) {
 			int size = ThreadLocalRandom.current().nextInt(MIN_SIZE, MAX_SIZE + 1);
-			List<Integer> items = randomList(size);
+			List<Integer> keys = randomList(size);
 
 			givenATree();
-			initializedWith(items);
+			initializedWith(keys);
 
-			Collections.sort(items);
-			traversingReturns(BinarySearchTree::inorderIterative, items);
+			Collections.sort(keys);
+			traversingReturns(BinarySearchTree::inorderIterative, keys);
+		}
+	}
+
+	public void testTraversal(Function<BinarySearchTree, List<Integer>> traverser, List<Integer> insertItems, List<Integer> expectedTraversedItems) {
+		givenATree();
+		initializedWith(insertItems);
+		traversingReturns(traverser, expectedTraversedItems);
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void preorder() {
+		Pair<List<Integer>, List<Integer>>[] parameters = new Pair[] {
+				Pair.of(Collections.emptyList(), Collections.emptyList()),
+				Pair.of(asList(1, 2, 3, 4, 5, 6, 7), asList(1, 2, 3, 4, 5, 6, 7)),
+				Pair.of(asList(7, 6, 5, 4, 3, 2, 1), asList(7, 6, 5, 4, 3, 2, 1)),
+				Pair.of(asList(40, 20, 60, 10, 30, 50, 70), asList(40, 20, 10, 30, 60,50, 70)),
+				Pair.of(asList(40, 20, 30), asList(40, 20, 30)),
+				Pair.of(asList(40, 60, 50), asList(40, 60, 50)),
+		};
+		
+		Arrays.stream(parameters)
+			  .forEach(parameter -> testTraversal(BinarySearchTree::preorder, parameter.getLeft(), parameter.getRight()));
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Test
+	public void postorder() {
+		Pair<List<Integer>, List<Integer>>[] parameters = new Pair[] {
+				Pair.of(Collections.emptyList(), Collections.emptyList()),
+				Pair.of(asList(1, 2, 3, 4, 5, 6, 7), asList(7, 6, 5, 4, 3, 2, 1)),
+				Pair.of(asList(7, 6, 5, 4, 3, 2, 1), asList(1, 2, 3, 4, 5, 6, 7)),
+				Pair.of(asList(40, 20, 60, 10, 30, 50, 70), asList(10, 30, 20, 50, 70, 60, 40)),
+				Pair.of(asList(40, 20, 30), asList(30, 20, 40)),
+				Pair.of(asList(40, 60, 50), asList(50, 60, 40)),
+		};
+		
+		Arrays.stream(parameters)
+		  .forEach(parameter -> testTraversal(BinarySearchTree::postorder, parameter.getLeft(), parameter.getRight()));
+	}
+	
+	@Test
+	public void contains() {
+		List<Integer> keys = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+		for (int i = 1; i <= 5; i++) {
+			Collections.shuffle(keys);
+			givenATree();
+			initializedWith(keys);
+			keys.stream().forEach(key -> assertTrue("tree contains " + key, tree.contains(key)));
+			
+			assertFalse("tree doesn't contain key", tree.contains(100));
+		}
+	}
+	
+	@Test
+	public void containsIterative() {
+		List<Integer> keys = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+
+		for (int i = 1; i <= 5; i++) {
+			Collections.shuffle(keys);
+			givenATree();
+			initializedWith(keys);
+			keys.stream().forEach(key -> assertTrue("tree contains " + key, tree.containsIterative(key)));
+			
+			assertFalse("tree doesn't contain key", tree.containsIterative(100));
+		}
+	}
+	
+	@Test
+	public void min() {
+		List<Integer> keys = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+
+		for (int i = 1; i <= 5; i++) {
+			Collections.shuffle(keys);
+			givenATree();
+			initializedWith(keys);
+			keys.stream().forEach(key -> assertThat("1 is min", tree.min(), equalTo(1)));
+		}
+	}
+	
+	@Test
+	public void max() {
+		List<Integer> keys = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+
+		for (int i = 1; i <= 5; i++) {
+			Collections.shuffle(keys);
+			givenATree();
+			initializedWith(keys);
+			
+			keys.stream().forEach(key -> assertThat("7 is max", tree.max(), equalTo(7)));
+		}
+	}
+	
+	@Test
+	public void successor() {
+		List<Integer> keys = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+		Map<Integer, Integer> successors = keys.stream().collect(Collectors.toMap(Function.identity(), n -> n + 1));
+		successors.put(keys.get(keys.size() - 1), null); // last item has no successor
+		
+		for (int i = 1; i <= 5; i++) {
+			Collections.shuffle(keys);
+			givenATree();
+			initializedWith(keys);
+			keys.stream().forEach(key -> assertThat(String.format("Successor of %s is %s", key, successors.get(key)), tree.successor(key), equalTo(successors.get(key))));
+		}
+	}
+	
+	@Test
+	public void predecessor() {
+		List<Integer> keys = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+		Map<Integer, Integer> predecessor = keys.stream().collect(Collectors.toMap(Function.identity(), n -> n - 1));
+		predecessor.put(keys.get(0), null); // first items has no predecessor
+		
+		for (int i = 1; i <= 5; i++) {
+			Collections.shuffle(keys);
+			givenATree();
+			initializedWith(keys);
+			keys.stream().forEach(key -> assertThat(String.format("Predecessor of %s is %s", key, predecessor.get(key)), tree.predecessor(key), equalTo(predecessor.get(key))));
+		}
+	}
+	
+	@Test
+	public void delete() {
+				
+		for (int i = 1; i <= 5; i++) {
+			List<Integer> keys = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
+			List<Integer> sortedKeys = new ArrayList<>(keys);
+			Collections.sort(sortedKeys);
+			
+			Collections.shuffle(keys);
+			givenATree();
+			initializedWith(keys);
+			
+			ListIterator<Integer> iterator = sortedKeys.listIterator();
+			
+			while(iterator.hasNext()) {
+				tree.delete(iterator.next());
+				iterator.remove();
+				traversingReturns(BinarySearchTree::inorder, sortedKeys);
+			}
+			
+			assertTrue("New tree is empty", tree.isEmpty());
+			assertThat("New tree has size zero", tree.size(), equalTo(0));
 		}
 	}
 }
